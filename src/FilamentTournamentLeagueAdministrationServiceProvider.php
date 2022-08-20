bbc7814c9f1860d1729809244dec5ca9513f3e6a
Maggomann <marco.ehrt@googlemail.com>
2022-08-20 19:33:51 +0200

    wip

--- src/FilamentTournamentLeagueAdministrationServiceProvider.php
+++ src/FilamentTournamentLeagueAdministrationServiceProvider.php
@@ -3,20 +3,23 @@
 namespace Maggomann\FilamentTournamentLeagueAdministration;
 
 use Maggomann\FilamentTournamentLeagueAdministration\Commands\FilamentTournamentLeagueAdministrationCommand;
+use Maggomann\FilamentTournamentLeagueAdministration\Resources\FederationResource;
+use Maggomann\FilamentTournamentLeagueAdministration\Resources\LeagueResource;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class FilamentTournamentLeagueAdministrationServiceProvider extends PackageServiceProvider
 {
+    protected array $resources = [
+        FederationResource::class,
+        LeagueResource::class,
+    ];
+
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-tournament-league-administration')
+            ->hasConfigFile()
             ->hasMigration('create_filament_tournament_league_administration_tables')
             ->hasCommand(FilamentTournamentLeagueAdministrationCommand::class);
     }
