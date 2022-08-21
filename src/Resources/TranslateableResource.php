eb77f95b3e98c9aa415762b696151bc377bcc664
Maggomann <marco.ehrt@googlemail.com>
2022-08-21 16:40:06 +0200

    wip

--- src/Resources/TranslateableResource.php
+++ src/Resources/TranslateableResource.php
@@ -3,16 +3,12 @@
 namespace Maggomann\FilamentTournamentLeagueAdministration\Resources;
 
 use Filament\Resources\Resource;
+use Maggomann\FilamentModelTranslator\Contracts\TranslateableModels;
 use Maggomann\FilamentModelTranslator\Traits\HasTranslateableModels;
 
-class TranslateableResource extends Resource
+class TranslateableResource extends Resource implements TranslateableModels
 {
     use HasTranslateableModels;
 
     protected static ?string $translateableKey = 'filament-tournament-league-administration::';
-
-    public function transPackageKey(): ?string
-    {
-        return static::$translateableKey;
-    }
 }
