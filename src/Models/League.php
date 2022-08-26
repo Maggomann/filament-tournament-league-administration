842dcca27266f03d2eb3bf32f4b277fabb4c8382
Maggomann <marco.ehrt@googlemail.com>
2022-08-26 18:03:59 +0200

    wip

--- src/Models/League.php
+++ src/Models/League.php
@@ -5,8 +5,8 @@ namespace Maggomann\FilamentTournamentLeagueAdministration\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
-use Spatie\Tags\HasSlug;
 
 class League extends Model
 {
