bbe2924bdb8edeb2fc22ce5d48926f6451218907
Maggomann <marco.ehrt@googlemail.com>
2022-09-03 13:10:03 +0200

    fix, create player page

--- src/Resources/PlayerResource/Pages/CreatePlayer.php
+++ src/Resources/PlayerResource/Pages/CreatePlayer.php
@@ -3,6 +3,7 @@
 namespace Maggomann\FilamentTournamentLeagueAdministration\Resources\PlayerResource\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
+use Maggomann\FilamentTournamentLeagueAdministration\Resources\PlayerResource;
 
 class CreatePlayer extends CreateRecord
 {
