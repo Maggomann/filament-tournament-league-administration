b41cf3cfdb3c920bfa3ee8330d9c8a7763d5aeed
Maggomann <marco.ehrt@googlemail.com>
2022-09-05 07:11:04 +0200

    refactor: refactoring trait HasHideableLabel

--- src/Contracts/Tables/Traits/HasHideableLabel.php
+++ src/Contracts/Tables/Traits/HasHideableLabel.php
@@ -2,27 +2,70 @@
 
 namespace Maggomann\FilamentTournamentLeagueAdministration\Contracts\Tables\Traits;
 
+use Closure;
+
 trait HasHideableLabel
 {
-    protected ?string $originLabel = null;
+    protected ?string $hiddenLabel = null;
+
+    protected bool $hiddenLabelDisplayedInTooltip = false;
 
     public function hideLabel(): static
     {
-        if ($this->originLabel === null) {
-            $this->originLabel = $this->label;
+        return $this->markHiddenLabelWithLabelIfNotYetDone()
+            ->markLabelWithEmptyString();
+    }
+
+    public function hideLabellnTooltip(): static
+    {
+        return $this->markHiddenLabelDisplayedInTooltip()
+            ->hideLabel()
+            ->markTooltipWithHiddenLabel();
+    }
+
+    public function tooltip(string | Closure | null $tooltip): static
+    {
+        if ($this->isHiddenLabelDisplayedInTooltip()) {
+            return $this->hideLabellnTooltip();
         }
 
+        $this->tooltip = $tooltip;
+
+        return $this;
+    }
+
+    private function markHiddenLabelDisplayedInTooltip(): static
+    {
+        $this->hiddenLabelDisplayedInTooltip = true;
+
+        return $this;
+    }
+
+    private function markTooltipWithHiddenLabel(): static
+    {
+        $this->tooltip = $this->originLabel;
+
+        return $this;
+    }
+
+    private function markHiddenLabelWithLabelIfNotYetDone(): static
+    {
+        if ($this->hiddenLabel === null) {
+            $this->hiddenLabel = $this->label;
+        }
+
+        return $this;
+    }
+
+    private function markLabelWithEmptyString(): static
+    {
         $this->label = '';
 
         return $this;
     }
 
-    public function hideLabellnTooltip(): static
+    public function isHiddenLabelDisplayedInTooltip(): bool
     {
-        $this->hideLabel();
-
-        $this->tooltip($this->originLabel);
-
-        return $this;
+        return $this->hiddenLabelDisplayedInTooltip;
     }
 }
