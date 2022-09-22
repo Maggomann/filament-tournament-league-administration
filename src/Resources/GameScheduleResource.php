4535545aac69ae3d341d974613105a247eb96fd6
Maggomann <marco.ehrt@googlemail.com>
2022-09-22 20:52:08 +0200

    wip: anpassung des spielplanformulars

--- src/Resources/GameScheduleResource.php
+++ src/Resources/GameScheduleResource.php
@@ -2,7 +2,10 @@
 
 namespace Maggomann\FilamentTournamentLeagueAdministration\Resources;
 
+use Closure;
 use Filament\Forms\Components\Card;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Table;
@@ -12,8 +15,12 @@ use Illuminate\Database\Eloquent\Model;
 use Maggomann\FilamentTournamentLeagueAdministration\Contracts\Tables\Actions\DeleteAction;
 use Maggomann\FilamentTournamentLeagueAdministration\Contracts\Tables\Actions\EditAction;
 use Maggomann\FilamentTournamentLeagueAdministration\Contracts\Tables\Actions\ViewAction;
+use Maggomann\FilamentTournamentLeagueAdministration\Contracts\TranslatedSelectOption;
 use Maggomann\FilamentTournamentLeagueAdministration\Forms\Components\CardTimestamps;
+use Maggomann\FilamentTournamentLeagueAdministration\Models\Federation;
 use Maggomann\FilamentTournamentLeagueAdministration\Models\GameSchedule;
+use Maggomann\FilamentTournamentLeagueAdministration\Models\League;
+use Maggomann\FilamentTournamentLeagueAdministration\Models\Team;
 use Maggomann\FilamentTournamentLeagueAdministration\Resources\GameScheduleResource\Pages;
 
 class GameScheduleResource extends TranslateableResource
@@ -34,6 +41,74 @@ class GameScheduleResource extends TranslateableResource
                     ->schema([
                         TextInput::make('name')
                             ->label(GameSchedule::transAttribute('name'))
+                            ->columnSpan(2)
+                            ->required(),
+
+                        Select::make('federation_id')
+                            ->label(League::transAttribute('federation_id'))
+                            ->validationAttribute(League::transAttribute('federation_id'))
+                            ->options(Federation::all()->pluck('name', 'id'))
+                            ->placeholder(
+                                TranslatedSelectOption::placeholder(static::$translateablePackageKey.'translations.forms.components.select.placeholder.federation_id')
+                            )
+                            ->required()
+                            ->searchable()
+                            ->reactive()
+                            ->afterStateUpdated(
+                                function (Closure $set) {
+                                    $set('league_id', null);
+                                }),
+
+                        Select::make('league_id')
+                            ->label(Team::transAttribute('league_id'))
+                            ->validationAttribute(Team::transAttribute('league_id'))
+                            ->options(function (Closure $get, Closure $set, ?GameSchedule $record) {
+                                $federationId = $get('federation_id');
+
+                                if (! $record) {
+                                    if (! $federationId) {
+                                        return collect([]);
+                                    }
+
+                                    return Federation::with('leagues')
+                                        ->find($federationId)
+                                        ?->leagues
+                                        ?->pluck('name', 'id') ?? collect([]);
+                                }
+
+                                $recordFederationId = $record?->federation?->id;
+
+                                if ($federationId === null) {
+                                    $set('federation_id', $recordFederationId);
+                                    $federationId = $recordFederationId;
+                                }
+
+                                if ($recordFederationId === $federationId) {
+                                    return $record?->federation
+                                        ?->leagues
+                                        ?->pluck('name', 'id')
+                                        ?? collect([]);
+                                }
+
+                                return Federation::with('leagues')
+                                    ->find($federationId)
+                                    ?->leagues
+                                    ?->pluck('name', 'id') ?? collect([]);
+                            })
+                            ->placeholder(
+                                TranslatedSelectOption::placeholder(static::$translateablePackageKey.'translations.forms.components.select.placeholder.league_id')
+                            )
+                            ->required()
+                            ->reactive(),
+
+                        DateTimePicker::make('period_start')
+                            ->label(GameSchedule::transAttribute('period_start'))
+                            ->firstDayOfWeek(1)
+                            ->required(),
+
+                        DateTimePicker::make('period_end')
+                            ->label(GameSchedule::transAttribute('period_end'))
+                            ->firstDayOfWeek(1)
                             ->required(),
                     ])
                     ->columns([
