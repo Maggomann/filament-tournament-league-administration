2fcb7f5aa76a03819955bda781d611cd7d9e8cdb
Maggomann <marco.ehrt@googlemail.com>
2022-10-14 07:03:33 +0200

    wip: testing extended

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Maggomann\FilamentTournamentLeagueAdministration\Tests;
 
+use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
+use BladeUI\Icons\BladeIconsServiceProvider;
+use Filament\FilamentServiceProvider;
+use Filament\Forms\FormsServiceProvider;
+use Filament\Support\SupportServiceProvider;
+use Filament\Tables\TablesServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Livewire\LivewireServiceProvider;
 use Maggomann\FilamentTournamentLeagueAdministration\FilamentTournamentLeagueAdministrationServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Support\Str;
 
 class TestCase extends Orchestra
 {
@@ -20,17 +30,45 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
+            BladeIconsServiceProvider::class,
+            BladeHeroiconsServiceProvider::class,
+            LivewireServiceProvider::class,
+            FilamentServiceProvider::class,
+            FormsServiceProvider::class,
+            SupportServiceProvider::class,
+            TablesServiceProvider::class,
             FilamentTournamentLeagueAdministrationServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('app.key', Str::random(32));
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-tournament-league-administration_table.php.stub';
+        $this->dropAndCreateDatabase();
+
+        $migration = include __DIR__.'/../database/migrations/create_filament_tournament_league_administration_tables.php';
         $migration->up();
-        */
+
+    }
+
+    private function dropAndCreateDatabase(): void
+    {
+        if (app('env') !== 'testing') {
+            return;
+        }
+
+        $database = 'testing';
+
+        config()->set('database.default', 'mysql');
+        config()->set('database.connections.mysql.database', $database);
+        config()->set('database.connections.mysql.username', 'root');
+
+        Schema::getConnection()->getDoctrineSchemaManager()->dropDatabase("`{$database}`");
+
+        Schema::getConnection()->getDoctrineSchemaManager()->createDatabase("`{$database}`");
+
+        DB::reconnect('mysql');
     }
 }
