336e473d880288c81d49d8286e4c752297aa6561
Maggomann <marco.ehrt@googlemail.com>
2022-10-22 15:12:23 +0200

    wip: backend management for games

--- src/Resources/GameResource.php
+++ src/Resources/GameResource.php
@@ -3,9 +3,12 @@
 namespace Maggomann\FilamentTournamentLeagueAdministration\Resources;
 
 use Closure;
-use Filament\Forms\Components\Card;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Resources\Form;
 use Filament\Resources\Table;
 use Filament\Tables\Columns\TextColumn;
@@ -33,149 +36,193 @@ class GameResource extends TranslateableResource
     {
         return $form
             ->schema([
-                Card::make()
-                    ->schema([
-                        Select::make('game_schedule_id')
-                            ->relationship('gameSchedule', 'name')
-                            ->label(Game::transAttribute('game_schedule_id'))
-                            ->placeholder(
-                                TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'game_schedule_id')
-                            )
-                            ->reactive()
-                            ->afterStateUpdated(function (Closure $set) {
-                                $set('game_day_id', null);
-                                $set('home_team_id', null);
-                                $set('guest_team_id', null);
-                            }),
+                Tabs::make('Heading')
+                    ->tabs([
+                        Tab::make('Spielplan')
+                            ->icon('heroicon-o-bell')
+                            ->badge('s')
+                            ->schema([
+                                Select::make('game_schedule_id')
+                                    ->relationship('gameSchedule', 'name')
+                                    ->label(Game::transAttribute('game_schedule_id'))
+                                    ->placeholder(
+                                        TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'game_schedule_id')
+                                    )
+                                    ->reactive()
+                                    ->afterStateUpdated(function (Closure $set) {
+                                        $set('game_day_id', null);
+                                        $set('home_team_id', null);
+                                        $set('guest_team_id', null);
+                                    }),
 
-                        Select::make('game_day_id')
-                            ->label(Game::transAttribute('game_day_id'))
-                            ->validationAttribute(Game::transAttribute('game_day_id'))
-                            ->options(function (Closure $get, Closure $set, ?Game $record) {
-                                $gameScheduleId = $get('game_schedule_id');
+                                Select::make('game_day_id')
+                                    ->label(Game::transAttribute('game_day_id'))
+                                    ->validationAttribute(Game::transAttribute('game_day_id'))
+                                    ->options(function (Closure $get, Closure $set, ?Game $record) {
+                                        $gameScheduleId = $get('game_schedule_id');
 
-                                if (! $record) {
-                                    if (! $gameScheduleId) {
-                                        return collect([]);
-                                    }
+                                        if (! $record) {
+                                            if (! $gameScheduleId) {
+                                                return collect([]);
+                                            }
 
-                                    return GameSchedule::with('days')
-                                        ->find($gameScheduleId)
-                                        ?->days
-                                        ?->pluck('day', 'id') ?? collect([]);
-                                }
+                                            return GameSchedule::with('days')
+                                                ->find($gameScheduleId)
+                                                ?->days
+                                                ?->pluck('day', 'id') ?? collect([]);
+                                        }
 
-                                $recordGameScheduleById = $record->gameSchedule?->id;
+                                        $recordGameScheduleById = $record->gameSchedule?->id;
 
-                                if ($recordGameScheduleById === $gameScheduleId) {
-                                    return $record->gameSchedule
-                                        ?->days
-                                        ?->pluck('day', 'id')
-                                        ?? collect([]);
-                                }
+                                        if ($recordGameScheduleById === $gameScheduleId) {
+                                            return $record->gameSchedule
+                                                ?->days
+                                                ?->pluck('day', 'id')
+                                                ?? collect([]);
+                                        }
 
-                                return GameSchedule::with('days')
-                                    ->find($gameScheduleId)
-                                    ?->days
-                                    ?->pluck('day', 'id') ?? collect([]);
-                            })
-                            ->placeholder(
-                                TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'game_day_id')
-                            )
-                            ->required()
-                            ->searchable()
-                            ->reactive(),
+                                        return GameSchedule::with('days')
+                                            ->find($gameScheduleId)
+                                            ?->days
+                                            ?->pluck('day', 'id') ?? collect([]);
+                                    })
+                                    ->placeholder(
+                                        TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'game_day_id')
+                                    )
+                                    ->required()
+                                    ->searchable()
+                                    ->reactive(),
 
-                        Select::make('home_team_id')
-                            ->label(Game::transAttribute('home_team_id'))
-                            ->validationAttribute(Game::transAttribute('home_team_id'))
-                            ->options(function (Closure $get, Closure $set, ?Game $record) {
-                                $gameScheduleId = $get('game_schedule_id');
+                                DateTimePicker::make('started_at')
+                                    ->label(Game::transAttribute('started_at'))
+                                    ->firstDayOfWeek(1)
+                                    ->required(),
 
-                                if (! $record) {
-                                    if (! $gameScheduleId) {
-                                        return collect([]);
-                                    }
+                                DateTimePicker::make('ended_at')
+                                    ->label(Game::transAttribute('ended_at'))
+                                    ->firstDayOfWeek(1)
+                                    ->required(),
+                            ]),
+                        Tab::make('Mannschaften')
+                            ->icon('heroicon-o-users')
+                            ->schema([
+                                Select::make('home_team_id')
+                                    ->label(Game::transAttribute('home_team_id'))
+                                    ->validationAttribute(Game::transAttribute('home_team_id'))
+                                    ->options(function (Closure $get, Closure $set, ?Game $record) {
+                                        $gameScheduleId = $get('game_schedule_id');
 
-                                    return GameSchedule::with('teams')
-                                        ->find($gameScheduleId)
-                                        ?->teams
-                                        ?->pluck('name', 'id') ?? collect([]);
-                                }
+                                        if (! $record) {
+                                            if (! $gameScheduleId) {
+                                                return collect([]);
+                                            }
 
-                                $recordGameScheduleById = $record->gameSchedule?->id;
+                                            return GameSchedule::with('teams')
+                                                ->find($gameScheduleId)
+                                                ?->teams
+                                                ?->pluck('name', 'id') ?? collect([]);
+                                        }
 
-                                if ($recordGameScheduleById === $gameScheduleId) {
-                                    return $record->gameSchedule
-                                        ?->teams
-                                        ?->pluck('name', 'id')
-                                        ?? collect([]);
-                                }
+                                        $recordGameScheduleById = $record->gameSchedule?->id;
 
-                                return GameSchedule::with('teams')
-                                    ->find($gameScheduleId)
-                                    ?->teams
-                                    ?->pluck('name', 'id') ?? collect([]);
-                            })
-                            ->placeholder(
-                                TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'home_team_id')
-                            )
-                            ->required()
-                            ->searchable()
-                            ->reactive(),
+                                        if ($recordGameScheduleById === $gameScheduleId) {
+                                            return $record->gameSchedule
+                                                ?->teams
+                                                ?->pluck('name', 'id')
+                                                ?? collect([]);
+                                        }
 
-                        Select::make('guest_team_id')
-                            ->label(Game::transAttribute('guest_team_id'))
-                            ->validationAttribute(Game::transAttribute('guest_team_id'))
-                            ->options(function (Closure $get, Closure $set, ?Game $record) {
-                                $gameScheduleId = $get('game_schedule_id');
+                                        return GameSchedule::with('teams')
+                                            ->find($gameScheduleId)
+                                            ?->teams
+                                            ?->pluck('name', 'id') ?? collect([]);
+                                    })
+                                    ->placeholder(
+                                        TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'home_team_id')
+                                    )
+                                    ->required()
+                                    ->searchable()
+                                    ->reactive(),
 
-                                if (! $record) {
-                                    if (! $gameScheduleId) {
-                                        return collect([]);
-                                    }
+                                Select::make('guest_team_id')
+                                    ->label(Game::transAttribute('guest_team_id'))
+                                    ->validationAttribute(Game::transAttribute('guest_team_id'))
+                                    ->options(function (Closure $get, Closure $set, ?Game $record) {
+                                        $gameScheduleId = $get('game_schedule_id');
 
-                                    return GameSchedule::with('teams')
-                                        ->find($gameScheduleId)
-                                        ?->teams
-                                        ?->pluck('name', 'id') ?? collect([]);
-                                }
+                                        if (! $record) {
+                                            if (! $gameScheduleId) {
+                                                return collect([]);
+                                            }
 
-                                $recordGameScheduleById = $record->gameSchedule?->id;
+                                            return GameSchedule::with('teams')
+                                                ->find($gameScheduleId)
+                                                ?->teams
+                                                ?->pluck('name', 'id') ?? collect([]);
+                                        }
 
-                                if ($recordGameScheduleById === $gameScheduleId) {
-                                    return $record->gameSchedule
-                                        ?->teams
-                                        ?->pluck('name', 'id')
-                                        ?? collect([]);
-                                }
+                                        $recordGameScheduleById = $record->gameSchedule?->id;
 
-                                return GameSchedule::with('teams')
-                                    ->find($gameScheduleId)
-                                    ?->teams
-                                    ?->pluck('name', 'id') ?? collect([]);
-                            })
-                            ->placeholder(
-                                TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'guest_team_id')
-                            )
-                            ->required()
-                            ->searchable()
-                            ->reactive(),
+                                        if ($recordGameScheduleById === $gameScheduleId) {
+                                            return $record->gameSchedule
+                                                ?->teams
+                                                ?->pluck('name', 'id')
+                                                ?? collect([]);
+                                        }
 
-                        DateTimePicker::make('started_at')
-                            ->label(Game::transAttribute('started_at'))
-                            ->firstDayOfWeek(1)
-                            ->required(),
+                                        return GameSchedule::with('teams')
+                                            ->find($gameScheduleId)
+                                            ?->teams
+                                            ?->pluck('name', 'id') ?? collect([]);
+                                    })
+                                    ->placeholder(
+                                        TranslatePlaceholderSelectOption::placeholder(static::$translateablePackageKey, 'guest_team_id')
+                                    )
+                                    ->required()
+                                    ->searchable()
+                                    ->reactive(),
 
-                        DateTimePicker::make('ended_at')
-                            ->label(Game::transAttribute('ended_at'))
-                            ->firstDayOfWeek(1)
-                            ->required(),
-                    ])
-                    ->columns([
+                            ]),
+                        Tab::make('Punkte')
+                            ->icon('heroicon-o-calculator')
+                            ->schema([
+                                TextInput::make('home_points_legs')
+                                    ->label(Game::transAttribute('home_points_legs'))
+                                    ->numeric(),
+
+                                TextInput::make('guest_points_legs')
+                                    ->label(Game::transAttribute('guest_points_legs'))
+                                    ->numeric(),
+
+                                TextInput::make('home_points_games')
+                                    ->label(Game::transAttribute('home_points_games'))
+                                    ->numeric(),
+
+                                TextInput::make('guest_points_games')
+                                    ->label(Game::transAttribute('guest_points_games'))
+                                    ->numeric(),
+
+                            ]),
+                        Tab::make('Punkte nach Verlängerung')
+                            ->icon('heroicon-o-scale')
+                            ->schema([
+                                Toggle::make('has_an_overtime')
+                                    ->label(Game::transAttribute('has_an_overtime'))
+                                    ->columnSpan(2),
+
+                                TextInput::make('home_points_after_draw')
+                                    ->label(Game::transAttribute('home_points_after_draw'))
+                                    ->numeric(),
+
+                                TextInput::make('guest_points_after_draw')
+                                    ->label(Game::transAttribute('guest_points_after_draw'))
+                                    ->numeric(),
+                            ]),
+                    ])->columns([
                         'sm' => 2,
                     ])
-                    ->columnSpan(2),
+                    ->columnSpan(2)
+                    ->activeTab(1),
                 CardTimestamps::make((new Game)),
             ])
             ->columns(3);
