7659619212eef9975ab553f948717ebb6e229db1
Maggomann <marco.ehrt@googlemail.com>
2022-11-20 14:12:45 +0100

    wip: tests extended

--- tests/Application/Resources/FederationResourceTest.php
+++ tests/Application/Resources/FederationResourceTest.php
@@ -2,6 +2,9 @@
 <?php
 
 use Database\Factories\FederationFactory;
+use Maggomann\FilamentTournamentLeagueAdministration\Domain\Support\Tables\Actions\DeleteAction;
+use Maggomann\FilamentTournamentLeagueAdministration\Models\CalculationType;
+use Maggomann\FilamentTournamentLeagueAdministration\Models\Federation;
 use Maggomann\FilamentTournamentLeagueAdministration\Resources\FederationResource;
 use Maggomann\FilamentTournamentLeagueAdministration\Resources\FederationResource\RelationManagers\LeaguesRelationManager;
 use function Pest\Livewire\livewire;
@@ -28,4 +31,83 @@ it('can render all federation relation managers', function () {
     ])
         ->assertSuccessful()
         ->assertCanNotSeeTableRecords($federation->leagues);
+});
+
+it('can create a federtion', function () {
+    $calculationType = CalculationType::first();
+
+    livewire(FederationResource\Pages\CreateFederation::class)
+        ->fillForm([
+            'name' => 'Example',
+            'slug' => 'example',
+            'calculation_type_id' => $calculationType->id,
+        ])
+        ->call('create')
+        ->assertHasNoFormErrors();
+
+    tap(
+        Federation::where([
+            'name' => 'Example',
+            'slug' => 'example',
+            'calculation_type_id' => $calculationType->id,
+        ])->first(),
+        function (Federation $federation) use ($calculationType) {
+            $this->assertDatabaseHas(Federation::class, [
+                'id' => $federation->id,
+                'name' => 'Example',
+                'calculation_type_id' => $calculationType->id,
+            ]);
+        }
+    );
+});
+
+it('can save a federation', function () {
+    $calculationType = CalculationType::first();
+    $federation = FederationFactory::new()->create([]);
+
+    livewire(FederationResource\Pages\EditFederation::class, [
+        'record' => $federation->getRouteKey(),
+    ])
+        ->fillForm([
+            'name' => 'Edit Example',
+            'calculation_type_id' => $calculationType->id,
+        ])
+        ->call('save')
+        ->assertHasNoFormErrors();
+
+    tap(
+        $federation->refresh(),
+        function (Federation $federation) use ($calculationType) {
+            $this->assertDatabaseHas(Federation::class, [
+                'id' => $federation->id,
+                'name' => 'Edit Example',
+                'slug' => 'edit-example',
+                'calculation_type_id' => $calculationType->id,
+            ]);
+        }
+    );
+});
+
+it('can validate input for federation page', function () {
+    livewire(FederationResource\Pages\CreateFederation::class)
+        ->fillForm([
+            'name' => null,
+            'calculation_type_id' => null,
+        ])
+        ->call('create')
+        ->assertHasFormErrors([
+            'name' => 'required',
+            'calculation_type_id' => 'required',
+        ]);
+});
+
+it('can delete a federation', function () {
+    $federation = FederationFactory::new()->create([]);
+
+    livewire(FederationResource\Pages\EditFederation::class, [
+        'record' => $federation->getRouteKey(),
+    ])
+        ->callPageAction(DeleteAction::class);
+
+    $this->assertSoftDeleted($federation);
 });
