5840452f3353ce6eb7a334528f9a1087f8ce6f1c
Maggomann <marco.ehrt@googlemail.com>
2023-01-05 21:57:14 +0100

    refactor: TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,11 +12,9 @@ use Filament\SpatieLaravelTranslatablePluginServiceProvider;
 use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\LivewireServiceProvider;
 use Maggomann\FilamentTournamentLeagueAdministration\FilamentTournamentLeagueAdministrationServiceProvider;
 use Maggomann\FilamentTournamentLeagueAdministration\Tests\Database\Factories\UserFactory;
-use Maggomann\FilamentTournamentLeagueAdministration\Tests\Models\User;
 use Maggomann\LaravelAddressable\LaravelAddressableServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
@@ -26,7 +24,6 @@ use Spatie\MediaLibrary\MediaLibraryServiceProvider;
 abstract class TestCase extends BaseTestCase
 {
     // use LazilyRefreshDatabase;
-    // use RefreshDatabase;
 
     protected function setUp(): void
     {
@@ -64,21 +61,5 @@ abstract class TestCase extends BaseTestCase
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-    }
-
-    protected function getEnvironmentSetUp($app): void
-    {
-        // $app['config']->set('auth.providers.users.model', User::class);
-        // $app['config']->set('view.paths', array_merge(
-        //     $app['config']->get('view.paths'),
-        //     [__DIR__ . '/../resources/views'],
-        // ));
-
-        // $app['config']->set('database.default', 'sqlite');
-        // $app['config']->set('database.connections.sqlite', [
-        //     'driver' => 'sqlite',
-        //     'prefix' => '',
-        //     'database' => ':memory:',
-        // ]);
     }
 }
